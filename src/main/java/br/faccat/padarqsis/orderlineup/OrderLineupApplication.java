0c4c5ef5ac3a95eaedd36279ce919470d2a2d7c7
isado <isadorabellaguarda@gmail.com>
2024-06-09 14:28:10 -0300

    first commit

---
+++ src/main/java/br/faccat/padarqsis/orderlineup/OrderLineupApplication.java
@@ -0,0 +1,13 @@
+package br.faccat.padarqsis.orderlineup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderLineupApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderLineupApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/faccat/padarqsis/orderlineup/OrderLineupApplicationTests.java
@@ -0,0 +1,13 @@
+package br.faccat.padarqsis.orderlineup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderLineupApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
